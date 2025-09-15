f2bb99bcca0af0d8595fab50355e0f6589d46485
justarunsthing <justarunsthing@outlook.com>
2025-09-15 08:50:52 +0100

    Added Category model

---
+++ Models/Category.cs
@@ -0,0 +1,19 @@
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace ContactProMVC.Models
+{
+    public class Category
+    {
+        public int Id { get; set; }
+
+        [Required]
+        public string? AppUserId { get; set; }
+
+        [Required]
+        [Display(Name = "Category Name")]
+        public string? Name { get; set; }
+
+        // Navigation Properties
+        public virtual AppUser? AppUser { get; set; }
+    }
+}
