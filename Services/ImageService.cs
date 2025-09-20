99e0c5e2c3dddf3ed5e87e89f062de15f0944543
justarunsthing <justarunsthing@outlook.com>
2025-09-20 08:36:12 +0700

    Added private readonly variables

--- Services/ImageService.cs
+++ Services/ImageService.cs
@@ -4,6 +4,9 @@ namespace ContactProMVC.Services
 {
     public class ImageService : IImageService
     {
+        private readonly string[] suffixes = { "Bytes", "KB", "MB", "GB", "TB", "PB" };
+        private readonly string defaultImage = "image/DefaultContactImage.png";
+
         public string ConvertByteArrayToFile(byte[] fileData, string extension)
         {
             throw new NotImplementedException();
