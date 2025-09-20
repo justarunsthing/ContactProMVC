a5143e06e8aa4bbc1ca5ab171f76e2b5745ccb3d
justarunsthing <justarunsthing@outlook.com>
2025-09-20 08:33:22 +0700

    Added ImageService class implementing interface

---
+++ Services/ImageService.cs
@@ -0,0 +1,17 @@
+﻿using ContactProMVC.Interaces;
+
+namespace ContactProMVC.Services
+{
+    public class ImageService : IImageService
+    {
+        public string ConvertByteArrayToFile(byte[] fileData, string extension)
+        {
+            throw new NotImplementedException();
+        }
+
+        public Task<byte[]> ConvertFileToByteArrayAsync(IFormFile file)
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
