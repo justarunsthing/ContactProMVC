bbf4d96d3c51d41667381e28be28958a8c9460da
justarunsthing <justarunsthing@outlook.com>
2025-09-20 08:32:55 +0700

    Added IImageService interface

---
+++ Interaces/IImageService.cs
@@ -0,0 +1,8 @@
+﻿namespace ContactProMVC.Interaces
+{
+    public interface IImageService
+    {
+        Task<byte[]> ConvertFileToByteArrayAsync(IFormFile file);
+        string ConvertByteArrayToFile(byte[] fileData, string extension);
+    }
+}
