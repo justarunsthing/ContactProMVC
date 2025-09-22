e42f3e00b1a6781c553df8e84fbb90879d9b5899
justarunsthing <justarunsthing@outlook.com>
2025-09-22 21:30:32 +0545

    Added AddressBookService with IAddressBookService

---
+++ Interaces/IAddressBookService.cs
@@ -0,0 +1,15 @@
+﻿using ContactProMVC.Models;
+
+namespace ContactProMVC.Interaces
+{
+    public interface IAddressBookService
+    {
+        Task AddContactToCategoryAsync(int categoryId, int contactId);
+        Task<bool> IsContactOnCategoryAsync(int categoryId, int contactId);
+        Task<IEnumerable<Category>> GetUserCategoriesAsync(string userId);
+        Task<ICollection<int>> GetContactCategoriesIdsAsync(int contactId);
+        Task<ICollection<Category>> GetContactCategoriesAsync(int contactId);
+        Task RemoveContactFromCategoryAsync(int categoryId, int contactId);
+        IEnumerable<Contact> SearchContacts(string searchString, string userId);
+    }
+}
---
+++ Services/AddressBookService.cs
@@ -0,0 +1,43 @@
+﻿using ContactProMVC.Models;
+using ContactProMVC.Interaces;
+
+namespace ContactProMVC.Services
+{
+    public class AddressBookService : IAddressBookService
+    {
+        public Task AddContactToCategoryAsync(int categoryId, int contactId)
+        {
+            throw new NotImplementedException();
+        }
+
+        public Task<ICollection<Category>> GetContactCategoriesAsync(int contactId)
+        {
+            throw new NotImplementedException();
+        }
+
+        public Task<ICollection<int>> GetContactCategoriesIdsAsync(int contactId)
+        {
+            throw new NotImplementedException();
+        }
+
+        public Task<IEnumerable<Category>> GetUserCategoriesAsync(string userId)
+        {
+            throw new NotImplementedException();
+        }
+
+        public Task<bool> IsContactOnCategoryAsync(int categoryId, int contactId)
+        {
+            throw new NotImplementedException();
+        }
+
+        public Task RemoveContactFromCategoryAsync(int categoryId, int contactId)
+        {
+            throw new NotImplementedException();
+        }
+
+        public IEnumerable<Contact> SearchContacts(string searchString, string userId)
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
