4bebb1eaf31630c660e47d66a43c2c42133a1ebb
justarunsthing <justarunsthing@outlook.com>
2025-09-26 22:08:29 +0545

    Added virtual properties, commit #1 from Pokhara

--- Models/AppUser.cs
+++ Models/AppUser.cs
@@ -18,5 +18,8 @@ namespace ContactProMVC.Models
 
         [NotMapped]
         public string? FullName { get { return $"{FirstName} {LastName}"; } }
+
+        public virtual ICollection<Contact> Contacts { get; set; } = new HashSet<Contact>();
+        public virtual ICollection<Category> Categories { get; set; } = new HashSet<Category>();
     }
 }
