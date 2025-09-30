feb7cccd17232affbdd07eb4ad3b6943d3afa0de
justarunsthing <justarunsthing@outlook.com>
2025-09-30 20:21:11 +0545

    Updated Edit contact action

--- Controllers/ContactsController.cs
+++ Controllers/ContactsController.cs
@@ -168,12 +168,17 @@ namespace ContactProMVC.Controllers
                 return NotFound();
             }
 
-            var contact = await _context.Contacts.FindAsync(id);
+            var appUserId = _userManager.GetUserId(User);
+            var contact = await _context.Contacts.Where(c => c.Id == id && c.AppUserId == appUserId)
+                                                 .FirstOrDefaultAsync();
+
             if (contact == null)
             {
                 return NotFound();
             }
+
             ViewData["AppUserId"] = new SelectList(_context.Users, "Id", "Id", contact.AppUserId);
+            
             return View(contact);
         }
 
