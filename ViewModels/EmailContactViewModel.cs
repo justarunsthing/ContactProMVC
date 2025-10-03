78a6565478e34bee0ed972260e9ff1f38fdbedb9
justarunsthing <justarunsthing@outlook.com>
2025-10-03 09:00:37 +0545

    Added EmailContactViewModel

---
+++ ViewModels/EmailContactViewModel.cs
@@ -0,0 +1,10 @@
+﻿using ContactProMVC.Models;
+
+namespace ContactProMVC.ViewModels
+{
+    public class EmailContactViewModel
+    {
+        public Contact? Contact { get; set; }
+        public EmailData? EmailData { get; set; }
+    }
+}
