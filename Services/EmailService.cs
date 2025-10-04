f63b53233f7444a9beabef0e8f786ac7d1058d69
justarunsthing <justarunsthing@outlook.com>
2025-10-04 11:03:42 +0545

    Inject MailSettings

--- Services/EmailService.cs
+++ Services/EmailService.cs
@@ -1,9 +1,18 @@
-﻿using Microsoft.AspNetCore.Identity.UI.Services;
+﻿using ContactProMVC.Models;
+using Microsoft.Extensions.Options;
+using Microsoft.AspNetCore.Identity.UI.Services;
 
 namespace ContactProMVC.Services
 {
     public class EmailService : IEmailSender
     {
+        private readonly MailSettings _mailSettings;
+
+        public EmailService(IOptions<MailSettings> mailSettings)
+        {
+            _mailSettings = mailSettings.Value;
+        }
+
         public Task SendEmailAsync(string email, string subject, string htmlMessage)
         {
             throw new NotImplementedException();
