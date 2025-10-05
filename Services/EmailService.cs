d9474c962854a08db3246e4ed3a50f1ab22a6d49
justarunsthing <justarunsthing@outlook.com>
2025-10-05 09:20:27 +0545

    Implemented SendEmailAsync method

--- Services/EmailService.cs
+++ Services/EmailService.cs
@@ -1,4 +1,7 @@
-﻿using ContactProMVC.Models;
+﻿using MimeKit;
+using MailKit.Net.Smtp;
+using MailKit.Security;
+using ContactProMVC.Models;
 using Microsoft.Extensions.Options;
 using Microsoft.AspNetCore.Identity.UI.Services;
 
@@ -13,9 +16,45 @@ namespace ContactProMVC.Services
             _mailSettings = mailSettings.Value;
         }
 
-        public Task SendEmailAsync(string email, string subject, string htmlMessage)
+        public async Task SendEmailAsync(string email, string subject, string htmlMessage)
         {
-            throw new NotImplementedException();
+            var sender = _mailSettings.Email;
+            var mail = new MimeMessage
+            {
+                Sender = MailboxAddress.Parse(sender),
+                Subject = subject
+            };
+
+            foreach (var emailAddress in email.Split(";"))
+            {
+                mail.To.Add(MailboxAddress.Parse(emailAddress));
+            }
+
+            var emailBody = new BodyBuilder
+            {
+                HtmlBody = htmlMessage
+            };
+
+            mail.Body = emailBody.ToMessageBody();
+
+            using SmtpClient smtp = new();
+
+            try
+            {
+                var host = _mailSettings.Host;
+                var port = _mailSettings.Port;
+                var password = _mailSettings.Password;
+
+                await smtp.ConnectAsync(host, port, SecureSocketOptions.StartTls);
+                await smtp.AuthenticateAsync(sender, password);
+                await smtp.SendAsync(mail);
+                await smtp.DisconnectAsync(true);
+            }
+            catch (Exception ex)
+            {
+                var errorMessage = ex.Message;
+                throw;
+            }
         }
     }
 }
