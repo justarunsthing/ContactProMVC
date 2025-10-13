8db9b2850da12bf10347a5e225e2d6794a18cb7c
justarunsthing <justarunsthing@outlook.com>
2025-10-13 22:19:35 +0545

    Added EmailCategoryViewModel

---
+++ ViewModels/EmailCategoryViewModel.cs
@@ -0,0 +1,10 @@
+﻿using ContactProMVC.Models;
+
+namespace ContactProMVC.ViewModels
+{
+    public class EmailCategoryViewModel
+    {
+        public List<Contact>? Contacts { get; set; }
+        public EmailData? EmailData { get; set; }
+    }
+}
