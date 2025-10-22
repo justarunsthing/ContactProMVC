1c7fe0b5737884a9a8f8469212d5b94fb77e7392
justarunsthing <justarunsthing@outlook.com>
2025-10-22 08:10:27 +0100

    Added ConnectionHelper class

---
+++ Helpers/ConnectionHelper.cs
@@ -0,0 +1,10 @@
+﻿namespace ContactProMVC.Helpers
+{
+    public static class ConnectionHelper
+    {
+        public static string GetConnectionString(IConfiguration configuration)
+        {
+            return "";
+        }
+    }
+}
