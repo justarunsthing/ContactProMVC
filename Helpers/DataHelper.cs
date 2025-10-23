6b97afea7a4da9cc369690e662b3cb3376502ccc
justarunsthing <justarunsthing@outlook.com>
2025-10-23 08:18:00 +0100

    Added DataHelper class

---
+++ Helpers/DataHelper.cs
@@ -0,0 +1,17 @@
+﻿using ContactProMVC.Data;
+using Microsoft.EntityFrameworkCore;
+
+namespace ContactProMVC.Helpers
+{
+    public static class DataHelper
+    {
+        public static async Task ManageDataAsync(IServiceProvider serviceProvider)
+        {
+            // Get an instance of ApplicationDbContext
+            var dbContext = serviceProvider.GetRequiredService<ApplicationDbContext>();
+
+            // Apply any pending migrations
+            await dbContext.Database.MigrateAsync();
+        }
+    }
+}
