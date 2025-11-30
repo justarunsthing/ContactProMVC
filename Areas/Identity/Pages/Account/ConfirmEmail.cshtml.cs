016b3c6cd3f73e3be1f759f8dea4701c338ff2e1
arunpun <83875939+arunpun@users.noreply.github.com>
2025-11-30 19:45:39 +0000

    Tweaks and improvements

--- Areas/Identity/Pages/Account/ConfirmEmail.cshtml.cs
+++ Areas/Identity/Pages/Account/ConfirmEmail.cshtml.cs
@@ -45,7 +45,7 @@ namespace ContactProMVC.Areas.Identity.Pages.Account
 
             code = Encoding.UTF8.GetString(WebEncoders.Base64UrlDecode(code));
             var result = await _userManager.ConfirmEmailAsync(user, code);
-            StatusMessage = result.Succeeded ? "Thank you for confirming your email." : "Error confirming your email.";
+            StatusMessage = result.Succeeded ? "Thanks for confirming your email! You can now log in." : "Error confirming your email.";
             return Page();
         }
     }
