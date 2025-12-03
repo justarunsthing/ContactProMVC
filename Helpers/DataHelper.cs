d151b14ed26170a59cc0b65909607786afe32322
justarunsthing <justarunsthing@outlook.com>
2025-12-03 08:18:54 +0000

    Seed demo user

--- Helpers/DataHelper.cs
+++ Helpers/DataHelper.cs
@@ -1,4 +1,6 @@
 ﻿using ContactProMVC.Data;
+using ContactProMVC.Models;
+using Microsoft.AspNetCore.Identity;
 using Microsoft.EntityFrameworkCore;
 
 namespace ContactProMVC.Helpers
@@ -10,8 +12,45 @@ namespace ContactProMVC.Helpers
             // Get an instance of ApplicationDbContext
             var dbContext = serviceProvider.GetRequiredService<ApplicationDbContext>();
 
+            // Get an instance of UserManager
+            var userManager = serviceProvider.GetRequiredService<UserManager<AppUser>>();
+
             // Apply any pending migrations
             await dbContext.Database.MigrateAsync();
+
+            // Seed demo user
+            await SeedDemoUserAsync(userManager);
+        }
+
+        public static async Task SeedDemoUserAsync(UserManager<AppUser> userManager)
+        {
+            var demoUser = new AppUser
+            {
+                UserName = "demouser@contactpro.com",
+                FirstName = "Demo",
+                LastName = "User",
+                Email = "demouser@contactpro.com",
+                EmailConfirmed = true
+            };
+
+            try
+            {
+                var user = await userManager.FindByEmailAsync(demoUser.Email);
+
+                if (user == null )
+                {
+                    await userManager.CreateAsync(demoUser, "Password1!");
+                }
+            }
+            catch (Exception ex)
+            {
+                Console.WriteLine("************* ERROR *************");
+                Console.WriteLine("Error seeding demo user");
+                Console.WriteLine(ex.Message);
+                Console.WriteLine("**********************");
+
+                throw;
+            }
         }
     }
 }
