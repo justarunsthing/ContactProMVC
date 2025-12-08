c638b2555efbe1b377dd49f9954db175a6c0b25e
justarunsthing <justarunsthing@outlook.com>
2025-12-08 08:15:47 +0000

    Migration for IsDemoUser flag

---
+++ Migrations/20251208081504_AddFlag.cs
@@ -0,0 +1,29 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace ContactProMVC.Migrations
+{
+    /// <inheritdoc />
+    public partial class AddFlag : Migration
+    {
+        /// <inheritdoc />
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.AddColumn<bool>(
+                name: "IsDemoUser",
+                table: "AspNetUsers",
+                type: "boolean",
+                nullable: false,
+                defaultValue: false);
+        }
+
+        /// <inheritdoc />
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropColumn(
+                name: "IsDemoUser",
+                table: "AspNetUsers");
+        }
+    }
+}
