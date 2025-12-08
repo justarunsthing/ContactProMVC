dfa0a44de6a68c3e5a208762bfdfd556b39d3545
justarunsthing <justarunsthing@outlook.com>
2025-12-08 08:13:26 +0000

    Added IsDemoUser flag to AppUser model

--- Models/AppUser.cs
+++ Models/AppUser.cs
@@ -16,6 +16,8 @@ namespace ContactProMVC.Models
         [StringLength(50, ErrorMessage = "The {0} must be at least {2} and at max {1} characters long.", MinimumLength = 2)]
         public string? LastName { get; set; }
 
+        public bool IsDemoUser { get; set; }
+
         [NotMapped]
         public string? FullName { get { return $"{FirstName} {LastName}"; } }
 
