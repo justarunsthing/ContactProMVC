3c0a3ac0b11afabefae9db2bb4114429b2b52f74
justarunsthing <justarunsthing@outlook.com>
2025-12-09 08:28:00 +0000

    Clean up DataHelper class

--- Helpers/DataHelper.cs
+++ Helpers/DataHelper.cs
@@ -12,47 +12,8 @@ namespace ContactProMVC.Helpers
             // Get an instance of ApplicationDbContext
             var dbContext = serviceProvider.GetRequiredService<ApplicationDbContext>();
 
-            // Get an instance of UserManager
-            var userManager = serviceProvider.GetRequiredService<UserManager<AppUser>>();
-
             // Apply any pending migrations
             await dbContext.Database.MigrateAsync();
-
-            // Seed demo user
-            // await SeedDemoUserAsync(userManager, dbContext);
-        }
-
-        public static async Task SeedDemoUserAsync(UserManager<AppUser> userManager, ApplicationDbContext context)
-        {
-            var demoUser = new AppUser
-            {
-                UserName = "demouser@contactpro.com",
-                FirstName = "Demo",
-                LastName = "User",
-                Email = "demouser@contactpro.com",
-                EmailConfirmed = true
-            };
-
-            try
-            {
-                var user = await userManager.FindByEmailAsync(demoUser.Email);
-
-                if (user == null )
-                {
-                    await userManager.CreateAsync(demoUser, "Password1!");
-                    await SeedDemoCategoriesAsync(context, demoUser);
-                    await SeedDemoContactsAsync(context, demoUser);
-                }
-            }
-            catch (Exception ex)
-            {
-                Console.WriteLine("************* ERROR *************");
-                Console.WriteLine("Error seeding demo user");
-                Console.WriteLine(ex.Message);
-                Console.WriteLine("**********************");
-
-                throw;
-            }
         }
 
         public static async Task<AppUser> CreateDemoUserAsync(UserManager<AppUser> userManager, ApplicationDbContext context, string demoUserId)
