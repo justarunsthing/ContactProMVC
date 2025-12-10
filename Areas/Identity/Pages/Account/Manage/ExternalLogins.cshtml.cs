c9d063f694c5b8d0696d9695c5d6958e984d818e
justarunsthing <justarunsthing@outlook.com>
2025-12-10 08:32:50 +0000

    Lock additional identity manage pages for demo user

--- Areas/Identity/Pages/Account/Manage/ExternalLogins.cshtml.cs
+++ Areas/Identity/Pages/Account/Manage/ExternalLogins.cshtml.cs
@@ -2,19 +2,21 @@
 // The .NET Foundation licenses this file to you under the MIT license.
 #nullable disable
 
+using ContactProMVC.Models;
+using Microsoft.AspNetCore.Authentication;
+using Microsoft.AspNetCore.Authorization;
+using Microsoft.AspNetCore.Identity;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.RazorPages;
 using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Threading;
 using System.Threading.Tasks;
-using ContactProMVC.Models;
-using Microsoft.AspNetCore.Authentication;
-using Microsoft.AspNetCore.Identity;
-using Microsoft.AspNetCore.Mvc;
-using Microsoft.AspNetCore.Mvc.RazorPages;
 
 namespace ContactProMVC.Areas.Identity.Pages.Account.Manage
 {
+    [Authorize(Policy = "AppUser")]
     public class ExternalLoginsModel : PageModel
     {
         private readonly UserManager<AppUser> _userManager;
